d6f4c168a786b836f8080eb646739cffe23ff596
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-06-14 23:26:32 +0200

    authentication token

--- azure/README.md
+++ azure/README.md
@@ -1,1 +1,3 @@
 # IT Seems that Microsoft.Bing.Speech is very old
+
+# Current version says: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-apis
--- azure/speech-rec-rest-console/src/Program.cs
+++ azure/speech-rec-rest-console/src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace src
 {
@@ -7,6 +9,39 @@ namespace src
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Authentication
+    {
+        public static readonly string FetchTokenUri =
+            "https://westus.api.cognitive.microsoft.com/sts/v1.0";
+        private string subscriptionKey;
+        private string token;
+
+        public Authentication(string subscriptionKey)
+        {
+            this.subscriptionKey = subscriptionKey;
+            this.token = FetchTokenAsync(FetchTokenUri, subscriptionKey).Result;
+        }
+
+        public string GetAccessToken()
+        {
+            return this.token;
+        }
+
+        private async Task<string> FetchTokenAsync(string fetchUri, string subscriptionKey)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                UriBuilder uriBuilder = new UriBuilder(fetchUri);
+                uriBuilder.Path += "/issueToken";
+
+                var result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null);
+                Console.WriteLine("Token Uri: {0}", uriBuilder.Uri.AbsoluteUri);
+                return await result.Content.ReadAsStringAsync();
+            }
         }
     }
 }

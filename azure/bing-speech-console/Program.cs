952b26baacf2de0dc0a46903c8b24dcfb41bd6f2
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-06-14 22:04:51 +0200

    bing speech console

---
+++ azure/bing-speech-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bing_speech_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
